27ddbb2278d1a58f5e3f22ea63386ddc113982bc
AnVD2002 <anvuduc150702@gmail.com>
2025-03-30 23:19:51 +0700

    add mapper bug

--- AnVD_project/src/main/java/com/example/AnVD_project/Mapper/UserMapper.java
+++ AnVD_project/src/main/java/com/example/AnVD_project/Mapper/UserMapper.java
@@ -1,12 +1,12 @@
-//package com.example.AnVD_project.Mapper;
-//
-//import com.example.AnVD_project.DTO.UserInfoResponse;
-//import com.example.AnVD_project.Entity.User;
-//import org.mapstruct.Mapper;
-//import org.mapstruct.ReportingPolicy;
-//
-//
-//@Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
-//public interface UserMapper {
-//    User toEntity(UserInfoResponse response);
-//}
+package com.example.AnVD_project.Mapper;
+
+import com.example.AnVD_project.DTO.UserInfoResponse;
+import com.example.AnVD_project.Entity.User;
+import org.mapstruct.Mapper;
+import org.mapstruct.ReportingPolicy;
+
+
+@Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
+public interface UserMapper {
+    User toEntity(UserInfoResponse response);
+}
