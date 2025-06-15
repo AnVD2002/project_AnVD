12825d989d21fe91c851bb69779719f4764abff0
AnVD2002 <anvuduc150702@gmail.com>
2025-06-16 00:02:23 +0700

    commit day 160602 v1

--- AnVD_project/src/main/java/com/example/AnVD_project/service/Report/UserReportExcelServiceImpl.java
+++ AnVD_project/src/main/java/com/example/AnVD_project/service/Report/UserReportExcelServiceImpl.java
@@ -1,8 +1,31 @@
 package com.example.AnVD_project.service.Report;
 
+import com.example.AnVD_project.dto.response.report.ReportResponse;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+
+import static com.example.AnVD_project.until.Constant.ReportTemplatePath.USER_TEMPLATE;
 
 @Service
 public class UserReportExcelServiceImpl {
+    public ResponseEntity<ReportResponse> exportExcel() throws FileNotFoundException {
+        try {
+            InputStream templateStream = getClass().getResourceAsStream(USER_TEMPLATE);
 
+            if (templateStream == null) {
+                throw new FileNotFoundException(USER_TEMPLATE);
+            }
+            Workbook workbook = new XSSFWorkbook(templateStream);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return null;
+
+    }
 }
